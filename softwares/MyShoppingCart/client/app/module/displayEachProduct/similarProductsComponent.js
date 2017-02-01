44f184bfa91ab9c5f2d8ad3099e5959540b05f2e
ShravyaVanchanagiri <shravya.vanchanagiri@india.semanticbits.com>
2017-02-01 16:04:24 +0530

    Added component for displaying similar products

--- softwares/MyShoppingCart/client/app/module/displayEachProduct/similarProductsComponent.js
+++ softwares/MyShoppingCart/client/app/module/displayEachProduct/similarProductsComponent.js
@@ -1,34 +1,23 @@
 /**
  * Created by vanchanagiri shravya on 1/31/2017.
  */
-(function(){
+(function () {
     angular.module('product')
-        .component('similarProducts',{
-        bindings: {
-            datas: '='
-        },
-        templateUrl: 'partials/similarProducts.html',
-            controller:productDetailsController,
-            controllerAs:'pc'
-    });
-}());
-/*(function(){
-    angular.module('product')
-        .component('similarProducts',{
+        .component('similarProducts', {
+
             bindings: {
-                status: '=',
-                data: '='
+                similar: '=',
+                currentPage: '=',
+                pageSize: '=',
+                numberOfPages: '='
+
             },
-            templateUrl: '/app/partials/advices.html',
-            controller:renderAdvicesController,
-            controllerAs:'rac'
+            templateUrl: 'partials/similarProducts.html',
+            controller: productDetailsController,
+            controllerAs: 'spc'
         });
-    renderAdvicesController.$inject=['passUtilsService'];
-    function renderAdvicesController(passUtilsService){
-        var vm=this;
-        vm.viewPendingCardButton=viewPendingCardButton;
-        function viewPendingCardButton(status){
-            return passUtilsService.canViewPendingAdvice(status);
-        }
+    productDetailsController.$inject = [];
+    function productDetailsController() {
+        var vm = this;
     }
-}());*/
+}());
