4d28736c262637c70ec389b754a82b9ebb0a2d4c
shravya <shravya.vanchanagiri@india.semanticbits.com>
2017-02-06 16:32:11 +0530

    added load more comments

--- softwares/MyShoppingCart/client/app/module/displayEachProduct/eachProductDetailsController.js
+++ softwares/MyShoppingCart/client/app/module/displayEachProduct/eachProductDetailsController.js
@@ -9,10 +9,10 @@
         .controller("productDetailsController", productDetailsController);
 
 
-    productDetailsController.$inject = ['$http', '$rootScope', '$stateParams', 'productDetailsService'];
+    productDetailsController.$inject = ['$http', '$rootScope', '$stateParams', 'productDetailsService','$scope'];
     //$scope.value=150;
 
-    function productDetailsController($http, $rootScope, $stateParams, productDetailsService) {
+    function productDetailsController($http, $rootScope, $stateParams, productDetailsService,$scope) {
         productDetailsService.getProductDetails();
 
         var vm = this;
@@ -23,6 +23,8 @@
         vm.pageSize = 4;
 
 
+
+
         /*vm.numberOfPages=10;*/
         vm.productDetails = productDetailsService.getSelectedProduct($stateParams.id);
         vm.similarProducts = productDetailsService.getSimilarProduct(vm.productDetails.subType, vm.productDetails.name);
@@ -31,6 +33,17 @@
         console.log(".......................................................................................");
         console.log(vm.similarProducts.name);
         vm.numberOfPages=vm.similarProducts.length/vm.pageSize;
+        console.log("**********************************************************************************");
+        console.log(vm.productDetails);
+        vm.comments=vm.productDetails.comments;
+        vm.limit=1;
+        vm.loadMore=loadMore;
+        //vm.loadMore();
+
+        function loadMore() {
+            var increamented = vm.limit + 3;
+            vm.limit = increamented > vm.comments.length ? vm.comments.length : increamented;
+        };
     }
 })();
 
